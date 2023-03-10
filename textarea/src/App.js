0d02e329ac9c83d6e0fc5687f96f1defaa88a1c1
Vo Minh Duy <duycan300690@outlook.com.vn>
2023-03-10 13:39:50 +0700

    textarea

---
+++ textarea/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TextareaAutoResize from "./components/TextareaAutoResize";
+
+function App() {
+  return (
+    <div className="App">
+      <TextareaAutoResize></TextareaAutoResize>
+    </div>
+  );
+}
+
+export default App;
