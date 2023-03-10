3e21a27dd023fc901a2ac1cb3522fee7125fcbcc
Vo Minh Duy <duycan300690@outlook.com.vn>
2023-03-10 17:37:57 +0700

    Dropdown

---
+++ dropdown/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import Dropdown from "./components/Dropdown";
+
+function App() {
+  return (
+    <div className="p-5">
+      <Dropdown></Dropdown>
+    </div>
+  );
+}
+
+export default App;
