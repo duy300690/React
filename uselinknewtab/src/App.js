d1beed8b1cdd740399b5fab9dfd7dbcb2cfc8f21
duy300690 <duy300690@gmail.com>
2023-03-19 16:57:17 +0700

    useNewTab

---
+++ uselinknewtab/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Blog from "./components/Blog";
+
+function App() {
+  return (
+    <div className="App">
+      <Blog></Blog>
+    </div>
+  );
+}
+
+export default App;
