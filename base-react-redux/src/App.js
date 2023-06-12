ead34b8ef2e357a853810d82de8054d0fd6be703
Vo Minh Duy <duycan300690@outlook.com.vn>
2023-06-12 22:33:33 +0700

    design header

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -1,6 +1,11 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
 function App() {
-  return <div className="app-container">Hello World</div>;
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
